f05f810d6ea2bbe7af38546de362047b8d1c0678
rushilbh <ytgamingrb@gmail.com>
2021-05-22 11:28:49 +0530

    added functions

--- pro-c98-assignment.py
+++ pro-c98-assignment.py
@@ -1,19 +1,21 @@
-file1=input("Enter your file name:-")
-f=open(file1,'r')
-fileContents=f.read()
-print(fileContents)
+def swapping():
+    file1=input("Enter your file name:-")
+    f=open(file1,'r')
+    fileContents=f.read()
+    print(fileContents)
 
-file2=input("Enter file 2 pls:-")
-g=open(file2,'r')
-fileContents2=g.read()
-print(fileContents2)
+    file2=input("Enter file 2 pls:-")
+    g=open(file2,'r')
+    fileContents2=g.read()
+    print(fileContents2)
 
 
-f=open(file1,'w')
-g=open(file2,'w')
+    f=open(file1,'w')
+    g=open(file2,'w')
 
-f.write(fileContents2)
-g.write(fileContents)
+    f.write(fileContents2)
+    g.write(fileContents)
 
-f.close()
-g.close()
+    f.close()
+    g.close()
+swapping()
